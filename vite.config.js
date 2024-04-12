c251c93ac6c3b3cc02c775f8900e4e91abdd0e53
Шарангия Игорь <igor_korenovsk@mail.ru>
2024-04-12 23:03:45 +0300

    update url build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-      ? '/fnirsi-wav-explorer/' // prod
+      ? '/Fnirsi-Wav-Viewer/' // prod
       : '/', // dev
   plugins: [
     vue(),
